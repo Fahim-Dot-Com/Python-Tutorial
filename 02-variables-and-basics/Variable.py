4ec686eef4b960b391cab0039542415dea9d93ce
Fahim <fahimakhtar928@gmail.com>
2026-03-07 17:05:45 +0000

    Add variable examples and conditionals in Variable.py
    Added variable definitions and basic conditional statements to demonstrate variable types and usage.

---
+++ 02-variables-and-basics/Variable.py
@@ -0,0 +1,30 @@
+# Variable = A container for a value (string, integer, float, boolean)
+# A variable behaves as if it was the value it contains
+
+# Strings are letters
+first_name = "Dude"
+food = "pizza"
+email= "dude123@fake.com"
+
+#Integers are numbers
+age = 25
+quantity = 3
+num_of_students = 30
+
+# Float is a number with a decimal point
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+# Boolean is a value of either True or False
+
+is_student = True
+
+if is_student:
+	print("You are a student")
+else:
+	print("You are NOT a student")
+
+print(f"You are {age} years old")
+print(f"You are buying {quantity} items")
+print(f"Your class has {num_of_students} students")
